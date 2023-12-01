37ff9c91e554f4d0a11ee33e0f6e3acc7f9875b7
Michal Czech <michal.czech@smartlogic.com>
2023-12-01 23:29:18 +0100

    commit

---
+++ getNumber.groovy
@@ -0,0 +1,19 @@
+List sumPerLine = []
+new File("puzzle-input.txt").eachLine { line ->
+  List digits = []
+  char[] c_array = line.toCharArray()
+  c_array.each { c ->
+    if (Character.isDigit(c)){
+      digits.add(c)
+    }
+  }
+  println("First digit is ${digits[0]} and last digit is ${digits[-1]} in line: ${line}")
+  def sum = "${digits[0]}${digits[-1]}"
+  println("Sum is ${sum}")
+  sum = Integer.parseInt(sum)
+  sumPerLine.add(sum)
+}
+println(sumPerLine)
+println(sumPerLine.size())
+def finalSum = sumPerLine.sum()
+println(finalSum)
