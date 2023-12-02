15a82c34f5e8316623bb9b4e2f47377c5c1e8df4
Michal Czech <michal.czech@smartlogic.com>
2023-12-02 16:30:52 +0100

    fuck yeah!

--- day1/groovy/getNumber.groovy
+++ day1/groovy/getNumber.groovy
@@ -2,7 +2,7 @@
 
 List sumPerLine = []
 
-new File("puzzle-input.txt").eachLine { line ->
+new File("../puzzle-input.txt").eachLine { line ->
   def sum = main(line)
   sumPerLine.add(sum)
 }
@@ -24,42 +24,41 @@ def main(String line) {
     'eight':'8',
     'nine':'9'
   ]
-  List digits = []
-  Map letterDigits = [:]
-  char[] c_array = line.toCharArray()
-  c_array.each { c ->
-    if (Character.isDigit(c)){
-      def index = line.indexOf(c.toString())
-      letterDigits[index] = c
-    }
-  }
-  wordsAndValues.each { word, value -> 
-    Map indexes = [:]
-    indexes = checkIfWordIsInsideLine(line, word, value)
-    if (!indexes.isEmpty() ) {
-      letterDigits << indexes
-    }
-  }
-  Map sorted = letterDigits.sort { a,b -> a.key <=> b.key }
-  // println("${letterDigits} and sorted ${sorted}")
-  List sortedLetterDigits = sorted.values().toArray()
-  // println("${sortedLetterDigits} and size ${sortedLetterDigits.size()}")
 
-  println("First digit is ${sortedLetterDigits[0]} and last digit is ${sortedLetterDigits[-1]} in line: ${line}")
-  def sum = "${sortedLetterDigits[0]}${sortedLetterDigits[-1]}"
+  Map indexesOfNumbers = [:]
+  wordsAndValues.each { word, value -> 
+    Map indexesOfWordNumbers = [:]
+    indexesOfWordNumbers = checkIfWordIsInsideLine(line, word, value)
+    if (!indexesOfWordNumbers.isEmpty() ) {
+      indexesOfNumbers << indexesOfWordNumbers
+    }
+    Map digitIndexes = [:]
+    digitIndexes = checkIfWordIsInsideLine(line, value)
+    if (!digitIndexes.isEmpty()) {
+      indexesOfNumbers << digitIndexes
+    }
+  }
+  Map sorted = indexesOfNumbers.sort { a,b -> a.key <=> b.key }
+  // println("${indexesOfNumbers} and sorted ${sorted}")
+  List sortedIndexesOfNumbers = sorted.values().toArray()
+  // println("${sortedindexesOfNumbers} and size ${sortedindexesOfNumbers.size()}")
+
+  println("First digit is ${sortedIndexesOfNumbers[0]} and last digit is ${sortedIndexesOfNumbers[-1]} in line: ${line}")
+  def sum = "${sortedIndexesOfNumbers[0]}${sortedIndexesOfNumbers[-1]}"
   println("Sum is ${sum}")
   sum = Integer.parseInt(sum)
 }
 
-def checkIfWordIsInsideLine(String line, String word, String value) {
+def checkIfWordIsInsideLine(String line, String word, String value=null) {
+  def countedValue = value==null ? word : value
   Map map = [:]
   def matcher = (line =~ /$word/)
   if (matcher.find()) {
     def index = line.indexOf(word)
-    map[index] = value
+    map[index] = countedValue
     if (matcher.count > 1) {
       def index2 = line.lastIndexOf(word)
-      map[index2] = value
+      map[index2] = countedValue
     }
   }
   return map
@@ -73,6 +72,7 @@ assert main("1twothree") == 13
 assert main("5") == 55
 assert main("5l") == 55
 assert main("one") == 11
+assert main("two555two5") == 25
 
 assert main("two1nine") == 29
 assert main("eightwothree") == 83
