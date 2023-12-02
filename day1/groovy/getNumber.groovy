2227871354778499a32dfcd4c4ced831e02f36bf
Michal Czech <michal.czech@smartlogic.com>
2023-12-02 16:51:02 +0100

    put it in main

--- day1/groovy/getNumber.groovy
+++ day1/groovy/getNumber.groovy
@@ -1,15 +1,17 @@
 
 
-List sumPerLine = []
-
-new File("../puzzle-input.txt").eachLine { line ->
-  def sum = getFirstAndLastNumber(line)
-  sumPerLine.add(sum)
+static void main(String[] args) {
+  tests()
+  List sumPerLine = []
+  new File("../puzzle-input.txt").eachLine { line ->
+    def sum = getFirstAndLastNumber(line)
+    sumPerLine.add(sum)
+  }
+  println(sumPerLine)
+  println(sumPerLine.size())
+  def finalSum = sumPerLine.sum()
+  println(finalSum)
 }
-println(sumPerLine)
-println(sumPerLine.size())
-def finalSum = sumPerLine.sum()
-println(finalSum)
 
 
 def getFirstAndLastNumber(String line) {
@@ -64,23 +66,24 @@ def checkIfWordIsInsideLine(String line, String word, String value=null) {
   return map
 }
 
+def tests() {
+  assert getFirstAndLastNumber("two5two3334two") == 22
+  assert getFirstAndLastNumber("1two5three4") == 14
+  assert getFirstAndLastNumber("onetwo3") == 13
+  assert getFirstAndLastNumber("1twothree") == 13
+  assert getFirstAndLastNumber("5") == 55
+  assert getFirstAndLastNumber("5l") == 55
+  assert getFirstAndLastNumber("one") == 11
+  assert getFirstAndLastNumber("two555two5") == 25
 
-assert getFirstAndLastNumber("two5two3334two") == 22
-assert getFirstAndLastNumber("1two5three4") == 14
-assert getFirstAndLastNumber("onetwo3") == 13
-assert getFirstAndLastNumber("1twothree") == 13
-assert getFirstAndLastNumber("5") == 55
-assert getFirstAndLastNumber("5l") == 55
-assert getFirstAndLastNumber("one") == 11
-assert getFirstAndLastNumber("two555two5") == 25
+  assert getFirstAndLastNumber("two1nine") == 29
+  assert getFirstAndLastNumber("eightwothree") == 83
+  assert getFirstAndLastNumber("abcone2threexyz") == 13
+  assert getFirstAndLastNumber("xtwone3four") == 24
+  assert getFirstAndLastNumber("4nineeightseven2") == 42
+  assert getFirstAndLastNumber("zoneight234") == 14
+  assert getFirstAndLastNumber("7pqrstsixteen") == 76
 
-assert getFirstAndLastNumber("two1nine") == 29
-assert getFirstAndLastNumber("eightwothree") == 83
-assert getFirstAndLastNumber("abcone2threexyz") == 13
-assert getFirstAndLastNumber("xtwone3four") == 24
-assert getFirstAndLastNumber("4nineeightseven2") == 42
-assert getFirstAndLastNumber("zoneight234") == 14
-assert getFirstAndLastNumber("7pqrstsixteen") == 76
-
-List testValues = [29, 83, 13, 24, 42, 14, 76]
-assert testValues.sum() == 281
+  List testValues = [29, 83, 13, 24, 42, 14, 76]
+  assert testValues.sum() == 281
+}
